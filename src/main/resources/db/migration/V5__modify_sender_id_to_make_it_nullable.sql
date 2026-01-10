3ad717bc8e114967d96dcc5955ee19cf74824481
Yohannes Getachew <demisgech@gmail.com>
2026-01-11 01:41:38 +0300

    Modify migration V5.

--- src/main/resources/db/migration/V5__modify_sender_id_to_make_it_nullable.sql
+++ src/main/resources/db/migration/V5__modify_sender_id_to_make_it_nullable.sql
@@ -1,2 +1,12 @@
+-- 1. Drop the FK constraint
+ALTER TABLE securepay.transaction_participants
+    DROP FOREIGN KEY fk_tp_sender;
+
+-- 2. Modify the column
 ALTER TABLE securepay.transaction_participants
     MODIFY COLUMN sender_id BIGINT NULL;
+
+-- 3. Re-add the FK
+ALTER TABLE securepay.transaction_participants
+    ADD CONSTRAINT fk_tp_sender
+        FOREIGN KEY (sender_id) REFERENCES users(id);
