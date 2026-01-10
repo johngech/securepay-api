f4d0955edd528d6f11bc85f878332b67c5f06fa5
Yohannes Getachew <demisgech@gmail.com>
2026-01-11 02:45:16 +0300

    Enable CORS.

--- src/main/java/com/marakicode/securepay/auth/SecurityConfig.java
+++ src/main/java/com/marakicode/securepay/auth/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.marakicode.securepay.auth;
 
 import com.marakicode.securepay.common.SecurityRules;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -20,13 +22,20 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.List;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${allowedOrigins:*}")
+    private String allowedOrigins;  // default to "*" if env not set
 
     private final UserDetailsService userDetailsService;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
@@ -55,6 +64,7 @@ public class SecurityConfig {
         http.sessionManagement(customizer ->
                         customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfiguration())) // enable cors
                 .authorizeHttpRequests(requestCustomizer -> {
                     featureSecurityRules.forEach(securityRules -> securityRules.configure(requestCustomizer));
                     requestCustomizer.anyRequest().authenticated();
@@ -67,5 +77,20 @@ public class SecurityConfig {
                     });
                 });
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfiguration() {
+        var configuration = new CorsConfiguration();
+        List<String> origins = Arrays.asList(allowedOrigins.split(","));
+        configuration.setAllowedOrigins(origins);
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true); // if using cookies/auth headers
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
